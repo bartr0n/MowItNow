83ccc3b24473b521b356a2b8070521089fdcdb94
José Antonio Martín Sanchez <jmartin23@gmail.com>
2016-12-18 23:33:42 +0100

    Ajout d'une cache dans la classe ActionFactory

--- src/main/java/fr/mowitnow/util/ActionFactory.java
+++ src/main/java/fr/mowitnow/util/ActionFactory.java
@@ -1,20 +1,41 @@
 package fr.mowitnow.util;
 
-import fr.mowitnow.brain.actions.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import fr.mowitnow.brain.actions.ActionHandler;
+import fr.mowitnow.brain.actions.Forward;
+import fr.mowitnow.brain.actions.TurnLeft;
+import fr.mowitnow.brain.actions.TurnRight;
 import fr.mowitnow.model.Action;
 
 public class ActionFactory {
-	
-    public static ActionHandler getInstance(Action action) {
 
-        switch (action) {
-            case A:
-                return new Forward();
-            case D:
-                return new TurnRight();
-            default:
-                return new TurnLeft();
-        }
+	public static Map<Action, ActionHandler> handlerCache = new HashMap<>();
 
-    }
+	public static ActionHandler getInstance(Action action) {
+
+		// On cherche sur la cache de handlers
+		ActionHandler handler = handlerCache.get(action);
+		if (handler != null) {
+			return handler;
+		}
+
+		// ActionHandler non trouvé, on le crée et on le met dans la cache pour
+		// les utilisations suivantes
+		switch (action) {
+		case A:
+			handler = new Forward();
+			break;
+		case D:
+			handler = new TurnRight();
+			break;
+		default:
+			handler = new TurnLeft();
+		}
+
+		handlerCache.put(action, handler);
+
+		return handler;
+	}
 }
